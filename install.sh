c6f8379446ac1b1ff780f004fec770e251411bad
Dominic Perez <dominicp0309@gmail.com>
2020-06-22 18:44:10 +1000

    install

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 gcc -c -fpic bitarray.c && \
 gcc -shared -o libbitarray.so bitarray.o && \
-sudo cp ./libbitarray.so /lib && \
+sudo cp ./libbitarray.so /usr/lib && \
 sudo cp ./bitarray.h /usr/include && \
 rm *.so *.o
