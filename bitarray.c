748bdd95eb84116dbfda2bfb55ac65417a7c5952
Dominic Perez <dominicp0309@gmail.com>
2020-06-22 16:43:18 +1000

    types

--- bitarray.c
+++ bitarray.c
@@ -3,10 +3,10 @@
 struct bitarray_t
 {
         int *bits;
-        long size;
+        int64_t size;
 };
 
-bitarray_t *ba_init(long initial_size)
+bitarray_t *ba_init(int64_t initial_size)
 {
         bitarray_t *b = malloc(sizeof *b);
         b->size = (initial_size % 32 == 0) ? initial_size / 32 : (initial_size / 32) + 1;
@@ -14,9 +14,9 @@ bitarray_t *ba_init(long initial_size)
         return b;
 }
 
-int ba_set_bit(bitarray_t *b, long index)
+int ba_set_bit(bitarray_t *b, int64_t index)
 {
-        long i = index / 32;
+        int64_t i = index / 32;
         if (!b || index < 0 || i >= b->size)
                 return -1;
         b->bits[i] |= 1 << (index % 32);
@@ -27,14 +27,14 @@ int ba_set_all(bitarray_t *b)
 {
         if (!b)
                 return -1;
-        for (long i = 0; i < b->size; i++)
+        for (int64_t i = 0; i < b->size; i++)
                 b->bits[i] = ~0;
         return 0;
 }
 
-int ba_clear_bit(bitarray_t *b, long index)
+int ba_clear_bit(bitarray_t *b, int64_t index)
 {
-        long i = index / 32;
+        int64_t i = index / 32;
         if (!b || index < 0 || i >= b->size)
                 return -1;
         b->bits[i] &= ~(1 << (index % 32));
@@ -45,29 +45,29 @@ int ba_clear_all(bitarray_t *b)
 {
         if (!b)
                 return -1;
-        for (long i = 0; i < b->size; i++)
+        for (int64_t i = 0; i < b->size; i++)
                 b->bits[i] = 0;
         return 0;
 }
 
-int ba_flip_bit(bitarray_t *b, long index)
+int ba_flip_bit(bitarray_t *b, int64_t index)
 {
-        long i = index / 32;
+        int64_t i = index / 32;
         if (!b || index < 0 || i >= b->size)
                 return -1;
         b->bits[i] ^= 1 << (index % 32);
         return 0;
 }
 
-char ba_get_bit(bitarray_t *b, long index)
+char ba_get_bit(bitarray_t *b, int64_t index)
 {
-        long i = index / 32;
+        int64_t i = index / 32;
         if (!b || index < 0 || i >= b->size)
                 return -1;
         return (b->bits[i] >> (index % 32)) & 1;
 }
 
-long ba_size(bitarray_t *b)
+int64_t ba_size(bitarray_t *b)
 {
         return 32 * b->size;
 }
--- bitarray.h
+++ bitarray.h
@@ -2,17 +2,18 @@
 #define BITARRAY_H
 
 #include <stdlib.h>
+#include <stdint.h>
 
 typedef struct bitarray_t bitarray_t;
 
-bitarray_t *ba_init(long initial_size);
-int ba_set_bit(bitarray_t *b, long index);
+bitarray_t *ba_init(int64_t initial_size);
+int ba_set_bit(bitarray_t *b, int64_t index);
 int ba_set_all(bitarray_t *b);
-int ba_clear_bit(bitarray_t *b, long index);
+int ba_clear_bit(bitarray_t *b, int64_t index);
 int ba_clear_all(bitarray_t *b);
-int ba_flip_bit(bitarray_t *b, long index);
-char ba_get_bit(bitarray_t *b, long index);
-long ba_size(bitarray_t *b);
+int ba_flip_bit(bitarray_t *b, int64_t index);
+char ba_get_bit(bitarray_t *b, int64_t index);
+int64_t ba_size(bitarray_t *b);
 void ba_destroy(bitarray_t *b);
 
 #endif /* BITARRAY_H */
